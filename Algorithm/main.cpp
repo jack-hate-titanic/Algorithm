eb573d4a5bed9652518cca1976993d0710ab31bb
Y-wson <1002783067@qq.com>
2021-05-01 12:52:54 +0800

    feat:first commit

---
+++ Algorithm/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  Algorithm
+//
+//  Created by  森 王 on 2021/5/1.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
